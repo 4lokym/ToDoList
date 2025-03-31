2e33d218e0877233217153c430dba56ac42df045
4lokym <mykola.koshilko@gmail.com>
2025-03-31 16:10:14 +0200

    Factory functions and composition of base elements

--- src/index.js
+++ src/index.js
@@ -2,15 +2,57 @@ import "./style.css";
  
 const createListTool = function (type){
     return function (name){
-        return {toString: `"${name}" is a ${type}`, name: name, type: type};
+        return {toString: `"${name}" is a ${type}`, name: name, type: type, elements_array: []};
     };
 };
 
-const createToDoList = createListTool(type = "toDoList");
-const createNoteList = createListTool(type = "noteList");
-const createCheckList = createListTool(type = "chekList");
+const createToDoList = createListTool("toDoList");
+const createNoteList = createListTool("noteList");
+const createCheckList = createListTool("chekList");
+
+const baseElement = function(title, type){
+    function toString(){
+        return `${this.title} is a ${this.type}`
+    }
+
+    return {title, id: crypto.randomUUID(), type, toString};
+}
+
+const createCheckElement = function(title, description = ""){;
+    const myobj = {description};
+
+    Object.assign(myobj, baseElement(title, "checkElement"))
+
+    myobj.toString = function(){
+        return `"${myobj.title}" is a ${myobj.type}\nId: ${myobj.id}\nDescription: ${myobj.description}`
+    }
+    
+    return myobj;
+}
+
+const createTodoElement = function(title, description = "", dueDate = null, priority = "low"){;
+    const myobj = {dueDate, priority};
+    Object.assign(myobj, createCheckElement(title, description));
+    myobj.type = "todoElement";
+    myobj.toString = function(){
+           return `"${myobj.title}" is a ${myobj.type}\nId: ${myobj.id}\nDueDate: ${myobj.dueDate}\nPriority: ${myobj.priority}\nDescription: ${myobj.description}`
+        }
+
+    return myobj;
+}
+
+const createNoteElement = function(title, text = ""){;
+    const myobj = {text};
+    Object.assign(myobj, baseElement(title, "noteElement"));
+    myobj.toString = function(){
+        return `"${myobj.title}" is a ${myobj.type}\nId: ${myobj.id}\nTextContent: ${text}`;
+    }
+    
+    return myobj;
+}
 
 
 
-
-
+console.log(createNoteElement("myNoteElement").toString());
+console.log(createCheckElement("myCheckElement").toString());
+console.log(createTodoElement("myTOdolement").toString());
